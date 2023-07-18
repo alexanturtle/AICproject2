fc46051b9febd98ac97bdd9fdfa83911795ee6d4
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-17 20:49:51 -0700

    changed

---
+++ store.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `shop` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `location` text,
+  `item_type` text,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO `shop` 
+	(`location`, `item_type`) 
+VALUES
