f8044f66879ae05f6612372061c2f8f0aa06226f
Zoya Lou <91094135+YayaPpig@users.noreply.github.com>
2023-07-18 19:55:24 -0700

    made changes to tables according to new design document

--- store.sql
+++ store.sql
@@ -1,29 +1,31 @@
 CREATE TABLE IF NOT EXISTS `shop` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` text,
+  `shop_type` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `shop` 
-	(`name`);
+	(`name`, `shop_type`);
 VALUES
      (`Atdp_merch`);
 
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int NOT NULL AUTO_INCREMENT,
- `shop_id` int,
+  `shop_id` int,
   `item_type` text,
+  `price` int
   PRIMARY KEY (`id`)
 );
 INSERT INTO `item` 
-	(`shop_id`, `item_type`);
+	(`shop_id`, `item_type`, `price`);
 VALUES
      ();
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
-   `shop_id` int,
+  `shop_id` int,
   `user_name` text,
-    `password` text,
+  `password` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `admin` 
@@ -34,7 +36,7 @@ VALUES
 CREATE TABLE IF NOT EXISTS `customer` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` text,
-    `password` text
+  `password` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `customer` 
@@ -45,8 +47,8 @@ VALUES
 CREATE TABLE IF NOT EXISTS `purchased` (
   `id` int NOT NULL AUTO_INCREMENT,
   `customer_id` int,
-`item_id` int,
-`bought` text
+  `item_id` int,
+  `bought` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `purchased` 
