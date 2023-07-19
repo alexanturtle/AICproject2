b89c0da64b94fae45eb4886cccc4e94c704cce31
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-18 20:52:01 -0700

    update

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_DSN', 'mysql:host=localhost;dbname=rshrivastav'); 
+define('DB_USER', 'rshrivastav');
+define('DB_PASSWORD', 'TTU74705');
+?>
--- store.sql
+++ store.sql
@@ -1,27 +1,35 @@
 CREATE TABLE IF NOT EXISTS `shop` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` text,
+  `shop_type` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `shop` 
-	(`name`);
+	(`name`, `shop_type`);
 VALUES
-     (`Atdp_merch`);
+     (`Atdp_merch`, `merch_store`),
+    (`Atdp_books`, `book_store`),
+     (`Atdp_boba`, `boba_store`);
+
 
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int NOT NULL AUTO_INCREMENT,
- `shop_id` int,
+  `shop_id` int,
   `item_type` text,
+  `price` int
   PRIMARY KEY (`id`)
 );
 INSERT INTO `item` 
-	(`shop_id`, `item_type`);
+	(`shop_id`, `item_type`, `price`);
 VALUES
-     ();
+     (1, `clothes`, 25),
+    (2, `books`, 15),
+     (3, `boba`, 5);
+
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
-   `shop_id` int,
+  `shop_id` int,
   `user_name` text,
 `password` text,
   PRIMARY KEY (`id`)
@@ -34,7 +42,7 @@ VALUES
 CREATE TABLE IF NOT EXISTS `customer` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` text,
-    `password` text
+  `password` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `customer` 
@@ -45,8 +53,8 @@ VALUES
 CREATE TABLE IF NOT EXISTS `purchased` (
   `id` int NOT NULL AUTO_INCREMENT,
   `customer_id` int,
-`item_id` int,
-`bought` text
+  `item_id` int,
+  `bought` text
   PRIMARY KEY (`id`)
 );
 INSERT INTO `purchased` 
