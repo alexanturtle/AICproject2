8a3eccc6c66746ab97be521014b75b171f2333b4
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-20 16:12:05 -0700

    hi

--- itemsinstore.php
+++ itemsinstore.php
@@ -11,9 +11,11 @@ require_once "config.php";
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
     $getitem = $dbh->prepare("SELECT * FROM items");
     $getitem->execute();
-    $items = $getitem->fetch();
+    $items = $getitem->fetchAll();
+    //var_dump($items);
     echo"<p>Items</p><br>";
     foreach($items as $item){
+        //var_dump($item);
         echo"<div class='item'>";
         if(isset($item['item_image'])){
             echo"<img src='" . $item['item_image'] . "' alt='no image'>";
@@ -21,6 +23,7 @@ require_once "config.php";
         if(isset($item['item_name'])){
             echo"<p>" . $item['item_name'] . "</p><br>";
         }
+        //echo"heyy";
         echo"</div>";
     }
     ?> 
