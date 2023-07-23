92f8f1e1c742c5e78527137f223d247046fb6a72
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-22 19:37:27 -0700

    changes in value

--- Project2/store.sql
+++ Project2/store.sql
@@ -25,18 +25,14 @@ CREATE TABLE IF NOT EXISTS `items` (
 INSERT INTO `items` 
     (`shop_id`, `price`, `category`, `item_name`, `item_image`)
 VALUES
-  (1, 24, 'clothes', 'Pink sweatshirt', 'pinksweat.jpg'),
-  (1, 24, 'clothes', 'blue sweatshirt', 'bluesweat.jpg'),
- (1, 24, 'clothes', 'black sweatshirt', 'blacksweat.jpg'),
-   (1, 24, 'clothes', 'white sweatshirt', 'whitesweat.jpg'),
-  (1, 20, 'clothes', 'Pink shirt', 'pinkshirt.jpg'),
-  (1, 20, 'clothes', 'blue shirt', 'blueshirt.jpg'),
-  (1, 20, 'clothes', 'black shirt', 'blackshirt.jpg'),
-  (1, 20, 'boba', 'white shirt', 'pinkshirt.jpg'),
-  (2, 6, 'boba', 'thai milk tea', 'thai.jpg'),
-  (2, 6, 'boba', 'Matcha', 'macha.jpg'),
-  (2, 6, 'boba', 'Jasmine milk tea', 'jasmine.jpg'),
-  (2, 6, 'boba', 'Taro', 'taro.jpg');
+ (1, 6, 'boba', 'rose milk tea', 'rose.jpg'),
+   (1, 6, 'boba', 'earl grey milk tea', 'earl.jpg'),
+  (1, 6, 'boba', 'caramel milk tea', 'caramel.jpg'),
+  (1, 7, 'boba', 'strawberry milk tea', 'strawberry.jpg'),
+  (1, 6, 'boba', 'thai milk tea', 'thai.jpg'),
+  (1, 7, 'boba', 'Matcha', 'macha.jpg'),
+  (1, 6, 'boba', 'Jasmine milk tea', 'jasmine.jpg'),
+  (1, 6, 'boba', 'Taro', 'taro.jpg');
   
   
 
