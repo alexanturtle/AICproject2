5dde257309e8474bf441604a2eb0fb4923531869
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-23 21:03:12 -0700

    hi

--- Project2/storemenu.php
+++ Project2/storemenu.php
@@ -5,7 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <body>
-<p><a href="itemsinstore.php"><img src="download.png" style="width:50px;height:50px;"></a></p><br>
+<p><a href="itemsinstore.php"><img src="download.png" style="width:150px;height:150px;"></a></p><br>
 <h1>Boba Shop</h1>
 </body>
 </html>
