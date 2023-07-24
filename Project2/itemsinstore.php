27f1d2400013873f580080391679722404a31d32
alexanturtle <richardwan.ca@gmail.com>
2023-07-23 18:31:35 -0700

    update itemsinstore, added toppings and changed css

--- Project2/itemsinstore.php
+++ Project2/itemsinstore.php
@@ -5,8 +5,14 @@ require_once "config.php";
 <html>
 <head>
     <title>Cart</title>
+    <link rel="stylesheet" href="items.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 </head>
 <body>
+<div id="toppingpage" class="hide">
+        <h1>Toppings</h1>
+        <button type='button' id='back' class='button' onClick='exit()'>Exit</button>
+    </div>
 <?php
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
     $getitem = $dbh->prepare("SELECT * FROM items");
@@ -16,13 +22,23 @@ require_once "config.php";
     foreach($items as $item){
         echo"<div class='item'>";
         if(isset($item['item_image'])){
-            echo"<img src='" . $item['item_image'] . "' alt='no image'>";
+            echo"<img src='" . $item['item_image'] . "' alt='no image' class='image'>";
         }
         if(isset($item['item_name'])){
-            echo"<p>" . $item['item_name'] . "</p><br>";
+            echo"<p>" . $item['item_name'] . "</p>";
         }
+        echo"<button type='button' id='topping' class='button' onClick='topping()'>Order</button><br><br>";
         echo"</div>";
     }
     ?> 
+
+    <script>
+    function topping(){
+        $(".hide").removeClass("hide").addClass("see");
+    }
+    function exit(){
+        $(".see").removeClass("see").addClass("hide");
+    }
+    </script>
 </body>
 </html>
