5af3d2dbf4827f14a822c900c44d734ac12f287e
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-24 23:35:55 -0700

    Update cart.php

--- Project2/cart.php
+++ Project2/cart.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require_once "config.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +9,10 @@
 </head>
 <body>
     <h1>Cart</h1>
-    <form action="checkout.php" method="post">
-=          <div id = "cart">
-            <br><br>
-            <button type='button' id='checkout' class='button' onClick='navigateToCheckout()'>Buy</button><br>
-            <button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>
-            <br><br>
-          </div>
-    </form>
-    <?php
+     <?php
     try {
         $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-                $sth = $dbh->prepare("SELECT `items.item_name` from items INNER JOIN purchased ON items.id=purchased.item_id;"); 
+                $sth = $dbh->prepare("SELECT items.item_name FROM items INNER JOIN purchased ON items.id=purchased.item_id"); 
                 $sth->execute();
                 $items= $sth->fetchAll();
                 foreach($items as $item){
@@ -28,6 +24,14 @@
       echo "<p>Error: {$e->getMessage()}</p>";
     }
 ?>
+   <form action="checkout.php" method="post">
+=          <div id = "cart">
+            <br><br>
+            <button type='button' id='checkout' class='button' onClick='navigateToCheckout()'>Buy</button><br>
+            <button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>
+            <br><br>
+          </div>
+    </form>
     <script>
     function navigateToCheckout() {
             window.location.href = 'checkout.php';
