3130f986362cfead61b4c407dfd24dc1cbe125aa
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-24 20:00:33 -0700

    update

--- Project2/store.sql
+++ Project2/store.sql
@@ -36,7 +36,12 @@ VALUES
   (1, 7, 'snack', 'Popcorn Chicken', 'chicken.jpg'),
   (1, 6, 'snack', 'French Fries', 'fries.jpeg'),
   (1, 7, 'snack', 'Lobster Balls', 'lobster.jpeg'),
-  (1, 6, 'snack', 'Chicken Pot-stickers', 'pot.jpeg');
+  (1, 6, 'snack', 'Chicken Pot-stickers', 'pot.jpeg'),
+  (1, 5, 'smoothie', 'Strawberry smoothie', 'strawberrysm.jpg'),
+  (1, 5, 'smoothie', 'Banana smoothie', 'banana.jpg'),
+  (1, 5, 'smoothie', 'Chocolate', 'chocolate.jpg'),
+  (1, 5, 'smoothie', 'Mango smoothie', 'mango.jpg');
+
   
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
