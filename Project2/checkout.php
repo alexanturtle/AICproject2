946225d1cbd967224b9f72bfd3161d9763e0d42b
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-26 16:34:49 -0700

    Update checkout.php

--- Project2/checkout.php
+++ Project2/checkout.php
@@ -20,7 +20,7 @@ require_once "config.php";
             echo "<h1>You have purchased your items!</h1>";
             echo "<img id = 'image' src='download.png' alt='boba'><br>";
             echo "<p>Come back again soon</p><br>";
-            echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>"
+            echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>";
         } else {
             echo "<h1>You have no items in cart to purchase</h1>";
             echo "<img id = 'image' src='sadbob.png' alt='boba'><br>";
