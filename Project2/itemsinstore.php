7c87d022f5dcc1a59f1ee817e6d645df35e584c8
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-25 20:56:37 -0700

    Update itemsinstore.php

--- Project2/itemsinstore.php
+++ Project2/itemsinstore.php
@@ -105,8 +105,9 @@ require_once "config.php";
         $(".see").removeClass("see").addClass("hide");
     }
     function addToCart() {
-        confirm("Added")        
-        window.location.href = 'addtocart.php?id=' + itemid;
+        if (confirm("Added")) {
+            window.location.href = 'addtocart.php?id=' + itemid;
+        } 
     }
     function navigateToCart(){
         window.location.href = 'cart.php';
