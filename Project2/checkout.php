caa8cb4654c43b0bb1bbdac93d7e0168b2f599cf
riti8517 <134117479+riti8517@users.noreply.github.com>
2023-07-26 20:27:42 -0700

    update

--- Project2/checkout.php
+++ Project2/checkout.php
@@ -11,27 +11,40 @@ require_once "config.php";
 <body>
 <title>Checkout Page</title>
     <link rel="stylesheet" href="cart.css">
-    <!-- <?php
-    try {
-        $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-        $sth = $dbh->prepare("SELECT * FROM purchased"); 
-        $result = $sth->execute();
-        if ($result->rowCount() > 0) {
-            echo "<h1>You have purchased your items!</h1>";
-            echo "<img id = 'image' src='download.png' alt='boba'><br>";
-            echo "<p>Come back again soon</p><br>";
-            echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>";
-        } else {
-            echo "<h1>You have no items in cart to purchase</h1>";
-            echo "<img id = 'image' src='sadbob.png' alt='boba'><br>";
-            echo "<button type='button' id='items' class='button' onClick='navigateToItemsPage()'>Back to Items</button>";
-            echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>";
-        }
-        }catch (PDOException $e) {
-      echo "<p>Error: {$e->getMessage()}</p>";
-  }
-?> -->
-<h1>You have purchased your items!</h1>
+    <h1>You have purchased your items</h1>
+    <img id = "image" src="download.png" alt="boba"><br>
+    <p>Come back again soon</p><br>
+    <button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>
+</body>
+<script>
+    function navigateToHomePage() {
+            window.location.href = 'homepage.php';
+    }
+    </script>
+</html>
+<!-- //<body> -->
+<!-- <title>Checkout Page</title>
+    <link rel="stylesheet" href="cart.css">
+//     try {
+//         $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+//         $sth = $dbh->prepare("SELECT * FROM purchased"); 
+//         $result = $sth->execute();
+//         if ($result->rowCount() > 0) {
+//             echo "<h1>You have purchased your items!</h1>";
+//             echo "<img id = 'image' src='download.png' alt='boba'><br>";
+//             echo "<p>Come back again soon</p><br>";
+//             echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>";
+//         } else {
+//             echo "<h1>You have no items in cart to purchase</h1>";
+//             echo "<img id = 'image' src='sadbob.png' alt='boba'><br>";
+//             echo "<button type='button' id='items' class='button' onClick='navigateToItemsPage()'>Back to Items</button>";
+//             echo "<button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>";
+//         }
+//         }catch (PDOException $e) {
+//       echo "<p>Error: {$e->getMessage()}</p>";
+//   }
+// ?> -->
+<!-- <h1>You have purchased your items!</h1>
 <img id = "image" src="download.png" alt="boba"><br>
 <p>Come back again soon</p><br>
 <button type='button' id='logout' class='button' onClick='navigateToHomePage()'>Logout</button>
@@ -42,6 +55,5 @@ require_once "config.php";
     }
     function navigateToItemsPage() {
             window.location.href = 'itemsinstore.php';
-    }
-    </script>
+    </script>  -->
 </html>
